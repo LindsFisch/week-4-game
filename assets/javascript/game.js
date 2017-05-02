c13cc4eac552d10bc15372dbf80a45f1556f8f44
Lindsey Fischer <lindsfisch@gmail.com>
2017-05-02 18:20:03 -0500

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,6 +24,7 @@ var options = {
 }
 
 var userChoice;
+var enemyChoice;
 var player;
 var opponent;
 
@@ -37,15 +38,56 @@ function initializeGame () {
 }
 
 initializeGame();
-//first pick
+
+//click to pick player and opponent
 $(".choice").on("click", function () {
+	//pick user player
 	if (userChoice === false && enemyChoice === false) {
 		player = options[$(this).data("gg-type")];
 		$(this).appendTo("#arena");
+		$("#chars > .imgbox").appendTo("#enemies");
 		userChoice = true;
+		console.log(player);
+	//pick opponent
 	} else if (userChoice === true && enemyChoice === false) {
+		opponent = options[$(this).data("gg-type")];
+		$(this).appendTo("#lanai");
+		enemyChoice = true;
+		console.log(opponent);
+	}
+
+})
+
+//click to attack
+$(".attack").on("click", function () {
+	//continue to attack
+	if (player.health > 0 && opponent.health > 0) {
+
+	//player won - new opponent
+	} else if (player.health > 0 && opponent.health <= 0) {
+
+
+		//if there are opponents remaining
+		if () {
+
+		//if there are no opponents remaining	
+		} else {	
+
+
+		//create restart button
+		}
+
+	//opponent won
+	} else if (player.health < 0 && opponent.health > 0){
 
 	}
+	console.log(player.health);
+	console.log(opponent.health);
+
+})
+
+//click to restart 
+$(".restart").on("click", function () {
 
 })
 });
